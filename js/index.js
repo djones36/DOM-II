1e41b78fe22119c274d36bba17f3a72d6a909670
Daniel Jones <daniel.jones2000@gmail.com>
2019-08-06 14:39:52 -0700

    4 events

--- js/index.js
+++ js/index.js
@@ -1,15 +1,31 @@
 // Your code goes here
 
-// 1. Drag an image and it will send an alert message
+// 1. Drag an image and it will send an alert message.
 let allImgs = document.querySelector('.img-content')
 allImgs.addEventListener('dragstart', drag => alert('Stealing is Bad!!'))
 
-//2. 
+//2. mouse out changes color to blue.
 let navBar = document.querySelector('nav')
 navBar.addEventListener('mouseout', event => {event.target.style.color = "blue"})
 
-//3.
+//3. double click to hide bus imge.
 let busImg = document.querySelector('.home .intro img')
 busImg.addEventListener('dblclick', displayNon =>{
     busImg.style.display = "none"
 })
+
+//4.
+// let bodyP = document.querySelectorAll('h1')
+// bodyP.addEventListener('scroll', changeColor =>{
+//     bodyP.style.color = "red"
+// })
+
+
+
+//5. tells you window size on resizing.
+window.addEventListener('resize', event => {
+    alert(`The window size is at ${window.innerHeight} by ${window.innerWidth}`)
+})
+
+
+//6.
