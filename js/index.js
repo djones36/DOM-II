98ce0ef62fb9080dcb76968d53eece8d9148fef9
Daniel Jones <daniel.jones2000@gmail.com>
2019-08-06 13:28:19 -0700

    added one event hadnler

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
 // Your code goes here
+
+// 1. Drag an image and it will send an alert message
 let allImgs = document.querySelector('.img-content')
 allImgs.addEventListener('dragstart', drag => alert('Stealing is Bad!!'))
+
+//2. 
+let navBar = document.querySelector('nav')
+navBar.addEventListener('mouseout', event => {event.target.style.color = "blue"})
+
+//3.
+let busImg = document.querySelector('.home .intro img')
+busImg.addEventListener('dblclick', displayNon =>{
+    busImg.style.display = "none"
+})
